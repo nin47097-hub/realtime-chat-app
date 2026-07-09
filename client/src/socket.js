cb2e44fa28940c61748ed05370a4cf1651214aaa
nin47097-hub <nin47097@gmail.com>
2026-07-09 19:05:22 +0530

    Connect frontend to deployed backend

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+
+const socket = io(
+    "https://realtime-chat-app-2-cwed.onrender.com"
+);
 
 export default socket;
