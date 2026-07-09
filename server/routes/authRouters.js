b987f0fc63357645d4fd3c2d0f5563a097399940
avneesh chatterjee <nin47097@gmail.com>
2026-07-09 11:06:58 +0530

    Update authRouters.js

--- server/routes/authRouters.js
+++ server/routes/authRouters.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { signupUser, loginUser } = require("../controllers/authController");
+const { signupUser, loginUser } = require("../controllers/authcontroller");
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
